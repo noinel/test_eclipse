8bdcdf3375bd54cf3d6b374a9be110130d811b93
jewon baek <rainbow0043.naver.com>
2019-04-23 12:05:52 +0900

    merge

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -5,11 +5,11 @@ public class test_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world~! hotfix!");
-<<<<<<< HEAD
+
 		System.out.println("i'm the master!");
-=======
+
 		System.out.println("i'm the hotfix!");
->>>>>>> refs/heads/hotfix
+
 	}
 
 }
